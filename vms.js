370006a5a631769b998fb9f8455072e0edb6fba0
KHOO YIE ZHE <yiezhe1234@gmial.com>
2023-06-16 17:25:32 +0800

    update 1

--- vms.js
+++ vms.js
@@ -1,1 +1,39 @@
-console.log("hello")
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://s2s3a:abc1234@record.55pqast.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+client.connect().then(res=>{
+  console.log(res)
+})
+
+function register(regUsername,regPassword,regEmail,regRole){
+    client.db("user").collection("visitor").insertOne({
+        username:regUsername,
+        password:regPassword,
+        email:regEmail,
+        role:regRole
+    })
+}
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Successfully connect to port ${port}`)
+  })
+
+app.post("/register" , (req, res) => {
+    res.send(register(req.body.username,req.body.password,req.body.email,req.body.role)) 
+    console.log(req.body.username,"successfully register")
+})
